b81516083a32eeeec7674ffc94757a9213d79b78
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2023-12-02 17:06:32 -0600

    fix(day-1): trim line

--- 1/part-1.ts
+++ 1/part-1.ts
@@ -1,16 +1,15 @@
 const input = await Bun.file(`${import.meta.dir}/input.txt`).text();
 
-const lines = input.split("\n");
+const result = input
+	.trim()
+	.split("\n")
+	.reduce((sum, line) => {
+		const characters = line.split("");
+		const numbers = characters.filter((character) =>
+			Number.isInteger(Number(character)),
+		);
 
-const result = lines.reduce((sum, line) => {
-	const characters = line.split("");
-	const numbers = characters.filter((character) =>
-		Number.isInteger(Number(character)),
-	);
-
-	return numbers.length > 0
-		? sum + Number(`${numbers[0]}${numbers.at(-1)}`)
-		: sum;
-}, 0);
+		return sum + Number(`${numbers[0]}${numbers.at(-1)}`);
+	}, 0);
 
 console.log(result);
